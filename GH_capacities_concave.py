d0c7b53f3c32dd787bb907c4f2c1c210ccb4bcb5
Mita Banik <mitabanik26@gmail.com>
2022-03-28 14:12:47 -0400

    Update GH_capacities_concave.py

--- GH_capacities_concave.py
+++ GH_capacities_concave.py
@@ -2,15 +2,18 @@ import numpy as np
 
 def GH_capacities_concave(it,x,f):
     # computing Gutt-Hutchings capacities upto it iterations for concave toric domains
-    cap=[]
-    for n in range(1,it+1):
-        Max=0;m=len(x);
-        for i in range(n):
-            A=[i,n-i+1]; Min=1e09;
-            Min=min(min(np.dot(A,[x,f])),Min)
+    # input : it = iterations ; x = point in [0,1] ; f = concave function 
+    # returns array of GH capacities
+    
+    cap = []
+    for n in range( 1 , it+1 ):
+        Max = 0; m = len( x );
+        for i in range( n ):
+            A = [i,n-i+1]; Min = 1e09;
+            Min = min(min ( np.dot( A, [ x , f] )), Min)
             
-            if Max<Min:
-                Max=Min
+            if Max < Min:
+                Max = Min
                 
-        cap.append(Max)
+        cap.append( Max )
     return cap
